f86aea9339fafa3b406ad581ccdebd33cd892e29
Palasa Navadeep <palasanavadeep@gmail.com>
2025-01-11 22:02:22 +0530

    refactor: restore catch-all route for serving index.html and clean up Vercel configuration

--- server2/src/app.js
+++ server2/src/app.js
@@ -60,9 +60,9 @@ app.use("/api/video",videoRouter);
 app.use("/api/dashboard",dashboardRouter);
 
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '..', 'public/dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/dist', 'index.html'));
+});
 
 
 
