3baae49577aea6cef58c2b1ce0faeae6365404b2
Palasa Navadeep <palasanavadeep@gmail.com>
2025-01-25 10:39:15 +0530

    fix : registration

--- server2/src/app.js
+++ server2/src/app.js
@@ -15,7 +15,7 @@ app.on("error" ,(err)=>{
     throw err
 })
 
-const allowedOrigins = [process.env.CORS_ORIGIN, "http://localhost:5173"];
+const allowedOrigins = [process.env.CORS_ORIGIN, "http://localhost:5173","http://localhost:8000"];
 
 app.use(cors({
     origin : allowedOrigins,
